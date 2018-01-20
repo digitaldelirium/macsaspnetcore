ccaeb1299ca5b58af86cbb52a99de65334509abd
Ian Cornett <ian.cornett@outlook.com>
2018-01-20 02:04:51 -0800

    remove secrets from appsettings.json for build engine replacement, build service using convention

--- Program.cs
+++ Program.cs
@@ -1,32 +1,47 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using System;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using MacsASPNETCore;
 using System.IO;
 using System.Net;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using MacsASPNETCore.Models;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.Azure.KeyVault;
+using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Remotion.Linq.Clauses.ResultOperators;
+using SQLitePCL;
+using static System.Environment;
 
 namespace MacsASPNETCore
 {
     public class Program
     {
-        private static IConfiguration Configuration { get; set; }
-
-        public Program(IConfiguration configuration)
+        private static IConfigurationRoot _configuration;
+        private static IHostingEnvironment _environment { get; set; }
+        private static X509Certificate2 _pfxCert { get; set; }
+        
+        public Program(IHostingEnvironment environment)
         {
-            Configuration = configuration;
+            _environment = environment;
         }
+        
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
-
-
-            using (var scope = host.Services.CreateScope())
+            
+            BuildWebHost(args).Run();
+            
+ /*           using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
 
@@ -42,24 +57,63 @@ namespace MacsASPNETCore
                     logger.LogError(e, "An error occurred seeding the DB.");
                     throw;
                 }
-            }
-
-            host.Run();
+            } */
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var builder = new WebHostBuilder();
+
+            var host = builder.ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("hosting.json", optional: false)
+                        .AddEnvironmentVariables();
+
+                    _configuration = config.Build();
+
+                });
+
+            var cert = GetCertificate(_environment);
+            if (!File.Exists(certPath))
+            {
+                Console.WriteLine(certPath);
+                Exit(4);
+            }
+
+                host.UseStartup<Startup>()
                 .UseKestrel(options =>
                 {
                     options.Listen(IPAddress.Loopback, 5000);
-                    options.Listen(IPAddress.Loopback, 5001, listenOptions =>
-                        {
-                            listenOptions.UseHttps("Macs.pfx", "Yb8b924VXG8S");
-                        });
+                    options.Listen(IPAddress.Loopback, 5001,
+                        listenOptions => { listenOptions.UseHttps(certPath, null); });
                 })
                 .UseIISIntegration()
-                .UseApplicationInsights()
-                .Build();
+                .UseApplicationInsights();
+
+            return host.Build();
+        }
+
+        private static object GetCertificate(IHostingEnvironment environment)
+        {
+            var pfx = new X509Certificate2();
+            if (environment.IsDevelopment())
+            {
+                string certPath = Directory.GetCurrentDirectory().ToString() + "Macs.pfx";
+                pfx = new X509Certificate2(certPath);
+            }
+            else
+            {
+                
+            }
+        }
+
+        public static async Task<string> GetToken(string authority, string res, string scope)
+        {
+            var auth = new AuthenticationContext(authority);
+            var cred = new ClientCredential(WebConfigurationManager.AppSettings["ClientId],
+                WebConfigurationManager.AppSettings["ClientSecret"]);
+            
+        }
     }
 }
