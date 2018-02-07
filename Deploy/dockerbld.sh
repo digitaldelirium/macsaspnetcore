d45ffb06f1f16a165ddcdc5688e2f557dbb94b8f
Ian Cornett <ian.cornett@outlook.com>
2018-02-06 21:36:47 -0800

    change to parent working directory

--- Deploy/dockerbld.sh
+++ Deploy/dockerbld.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 PASSWORD=$1
 
+cd ../
 docker build -t macscampingarea/macsapp --rm --compress .
 docker login --username macscampingarea --password $PASSWORD
 docker push --disable-content-trust macscampingarea/macsapp:prod
