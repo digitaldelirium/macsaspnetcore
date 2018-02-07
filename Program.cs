3361b923ef95cb29910dc4771e2a6cee67cc645e
Ian Cornett <ian.cornett@outlook.com>
2018-02-07 12:38:52 -0800

    remove system.web.configuration

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web.Configuration;
 using MacsASPNETCore.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting.Internal;
