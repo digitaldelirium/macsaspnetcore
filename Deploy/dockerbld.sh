34ef014c5f44ace9ca1c4cf8e4952857dac0de9e
Ian Cornett <ian.cornett@outlook.com>
2018-02-07 21:12:38 -0800

    remove commented push command

--- Deploy/dockerbld.sh
+++ Deploy/dockerbld.sh
@@ -3,5 +3,4 @@ PASSWORD=$1
 
 cd ../
 docker login --username macscampingarea --password ${PASSWORD}
-docker build -t macscampingarea/macsapp --rm --compress .
-# docker push --disable-content-trust macscampingarea/macsapp:prod
+docker build -t macscampingarea/macsapp --rm .
