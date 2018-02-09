38b189db0ea7d2b74814b6a4a5e2744bdeedaaa6
Ian Cornett <digitaldelirium@users.noreply.github.com>
2018-02-09 11:59:43 -0800

    move to cloudflare cert
    move to long-term cloudflare cert

--- Program.cs
+++ Program.cs
@@ -166,7 +166,7 @@ namespace MacsASPNETCore
             {
                 var kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetToken));
                 var secret = await kvClient
-                    .GetSecretAsync("https://macscampvault.vault.azure.net/secrets/letsencrypt").ConfigureAwait(false);
+                    .GetSecretAsync("https://macscampvault.vault.azure.net/secrets/macsvmssl").ConfigureAwait(false);
 
                 byte[] bytes;
                 if(secret.ContentType == "application/x-pkcs12")
