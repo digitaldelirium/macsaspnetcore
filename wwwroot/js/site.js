9da4d4b426acee42ea9fc036f551c3a0df1576ef
Ian Cornett <ian.cornett@outlook.com>
2018-02-25 04:22:24 -0800

    attempt to collapse menu, add font-awesome to project

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 function createIframe(parentId) {
     $(parentId).append("<div class='col-md-12 col-lg-12' id='iframeDiv'><iframe  id='generatedIframe'></iframe></div>");
 }
+
+var collapse = (function () {
+    $(document).ready(function () {
+        if ($(document).width > 768 && $(document).width < 992){
+            $(".collapse").collapse("hide");
+        }
+    });
+
+
+
+})();
