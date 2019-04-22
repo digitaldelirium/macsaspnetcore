7d839602b160c0480f5a14e514597c260c60eef2
Ian Cornett <ian.cornett@outlook.com>
2019-04-22 00:05:02 -0700

    Cleanup Script
    Analyzed and tested by ISE Steroids

--- Deploy/Replace-Tokens.ps1
+++ Deploy/Replace-Tokens.ps1
@@ -1,27 +1,27 @@
 #! /usr/bin/pwsh
 param (
-    [Parameter(Mandatory = $true, HelpMessage = 'Client ID for SPN', ParameterSetName = 'SPN')]
+    [Parameter(Mandatory, HelpMessage = 'Client ID for SPN', ParameterSetName = 'SPN')]
     [ValidateNotNullOrEmpty()]
     [string]
     $ClientId,
 
-    [Parameter(Mandatory = $true, HelpMessage = 'Client Secret for SPN', ParameterSetName = 'SPN')]
+    [Parameter(Mandatory, HelpMessage = 'Client Secret for SPN', ParameterSetName = 'SPN')]
     [ValidateNotNullOrEmpty()]
     [string]
     $ClientSecret,
 
-    [Parameter(Mandatory = $true, HelpMessage = 'AAD Tenant', ParameterSetName = 'SPN')]
+    [Parameter(Mandatory, HelpMessage = 'AAD Tenant', ParameterSetName = 'SPN')]
     [ValidateNotNullOrEmpty()]
     [string]
     $TenantId,
 
-    [Parameter(Mandatory = $true, HelpMessage = "Key Vault Name", ParameterSetName = 'Default')]
-    [Parameter(Mandatory = $true, HelpMessage = "Key Vault Name", ParameterSetName = 'SPN')]        
+    [Parameter(Mandatory, HelpMessage = "Key Vault Name", ParameterSetName = 'Default')]
+    [Parameter(Mandatory, HelpMessage = "Key Vault Name", ParameterSetName = 'SPN')]        
     [ValidateNotNullOrEmpty()]
     [string]
     $VaultName,
 
-    [Parameter(Mandatory = $false, HelpMessage = "full or partial subscription name, if not default")]
+    [Parameter(HelpMessage = "full or partial subscription name, if not default")]
     [string]
     $SubscriptionId,
 
@@ -33,28 +33,28 @@ param (
 function Replace-Tokens {
     [CmdletBinding(DefaultParameterSetName = 'Default')]
     param (
-        [Parameter(Mandatory = $true, HelpMessage = 'Client ID for SPN', ParameterSetName = 'SPN')]
+        [Parameter(Mandatory, HelpMessage = 'Client ID for SPN', ParameterSetName = 'SPN')]
         [ValidateNotNullOrEmpty()]
         [string]
         $ClientId,
 
-        [Parameter(Mandatory = $true, HelpMessage = 'Client Secret for SPN', ParameterSetName = 'SPN')]
+        [Parameter(Mandatory, HelpMessage = 'Client Secret for SPN', ParameterSetName = 'SPN')]
         [ValidateNotNullOrEmpty()]
         [string]
         $ClientSecret,
 
-        [Parameter(Mandatory = $true, HelpMessage = 'AAD Tenant', ParameterSetName = 'SPN')]
+        [Parameter(Mandatory, HelpMessage = 'AAD Tenant', ParameterSetName = 'SPN')]
         [ValidateNotNullOrEmpty()]
         [string]
         $TenantId,
 
-        [Parameter(Mandatory = $true, HelpMessage = "Key Vault Name", ParameterSetName = 'Default')]
-        [Parameter(Mandatory = $true, HelpMessage = "Key Vault Name", ParameterSetName = 'SPN')]        
+        [Parameter(Mandatory, HelpMessage = "Key Vault Name", ParameterSetName = 'Default')]
+        [Parameter(Mandatory, HelpMessage = "Key Vault Name", ParameterSetName = 'SPN')]        
         [ValidateNotNullOrEmpty()]
         [string]
         $VaultName,
 
-        [Parameter(Mandatory = $false, HelpMessage = "full or partial subscription name, if not default")]
+        [Parameter(HelpMessage = "full or partial subscription name, if not default")]
         [string]
         $SubscriptionId,
 
@@ -78,17 +78,17 @@ function Replace-Tokens {
             Set-AzContext -Subscription $SubscriptionId -Tenant $TenantId
         }
 
-        $global:kvSecrets = Get-AzKeyVaultSecret -VaultName $VaultName
+        $script:kvSecrets = Get-AzKeyVaultSecret -VaultName $VaultName
 
         $script:jsonFiles = Get-ChildItem -File -Path ./ -Filter '*.json'
         $script:csFiles = Get-ChildItem -File -Path ./ -Filter '*.cs'
 
-        $global:files = @($script:jsonFiles + $script:csFiles)
+        $script:files = @($script:jsonFiles + $script:csFiles)
     }
         
     process {
         $pfxFiles = $kvSecrets | Where-Object -FilterScript { $_.contentType -like 'application/x-pkcs12' } | Select-Object
-        $pfxFiles.ForEach({
+        $pfxFiles.ForEach{
                 $prefix = "https://$($VaultName).vault.azure.net/secrets/"
                 $name = $_.id.Substring($prefix.ToString().Length + 4)
                 Write-Host "$name"                
@@ -98,30 +98,37 @@ function Replace-Tokens {
                 $pfx.Import($pfxUnprotectedBytes, $null, [Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
                 $pfx.PrivateKey.ExportParameters($true)
                 [IO.File]::WriteAllBytes("$name.pfx", $pfxUnprotectedBytes)
-            })
+            }
 
-        $script:tokenExp = "${+[a-z]+\W*[a-z]*}+"
-        $secrets = @{}
-
-        $kvSecrets.Name.ForEach({
+        $kvSecrets.Name.ForEach{
             $secret = $_
             Write-Host "Looking for #{$secret}#"
-            $files.ForEach({
+            $files.ForEach{
                 $file = $_
-                $content = Get-Content $file
+                try
+                {
+                  $content = Get-Content $file
+                }
+                catch [Management.Automation.ItemNotFoundException]
+                {
+                  Write-Debug "Skipping file $file due to bad path"
+                }
+                
+                
+                
                 $matches = 0
                 
                 if($content -match "#{$secret}#"){
                     $matches++
-                    Write-Host "Replacing $file Values with $secret Secret Values"
+                    Write-Debug "Replacing $file Values with $secret Secret Values"
                     $content.replace("#{$secret}#", $(Get-AzKeyVaultSecret -Name $secret -VaultName $VaultName).SecretValueText) | Set-Content $file
                 }
 
                 if($matches -eq 0){
-                    Write-Host "No matches found for $file"
+                    Write-Debug "No matches found for $file"
                 }
-            })    
-        })
+            }    
+        }
 
     }
         
