d37173dfbbe0d749194edc221fc1fae9a5109c4c
Ian Cornett <ian.cornett@outlook.com>
2020-05-23 22:15:21 -0700

    add unexpected opening

---
+++ activity_Calendars.sql
@@ -0,0 +1,13 @@
+create table Calendars
+(
+    Id   int auto_increment
+        primary key,
+    Year int not null
+);
+
+INSERT INTO activity.Calendars (Id, Year) VALUES (4, 2016);
+INSERT INTO activity.Calendars (Id, Year) VALUES (5, 2017);
+INSERT INTO activity.Calendars (Id, Year) VALUES (6, 2018);
+INSERT INTO activity.Calendars (Id, Year) VALUES (7, 2019);
+INSERT INTO activity.Calendars (Id, Year) VALUES (8, 2020);
+INSERT INTO activity.Calendars (Id, Year) VALUES (9, 2021);
