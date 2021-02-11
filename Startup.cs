af654af2eb40ba54b11f04fe02645afa0c62b06d
Ian Cornett <ian.cornett@outlook.com>
2021-02-10 22:18:19 -0800

    remove https for app svc

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MacsASPNETCore.Models;
 using MacsASPNETCore.Services;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -58,7 +58,9 @@ namespace MacsASPNETCore
                             "http://macscampingarea.com",
                             "https://www.macscampingarea.com",
                             "http://www.macscampingarea.com",
-                            "https://connect.facebook.com"
+                            "https://connect.facebook.com",
+                            "https://macscampingarea.azurewebsites.net",
+                            "http://macscampingarea.azurewebsites.net"
                         )
                         .AllowAnyMethod()
                         .AllowAnyHeader();
@@ -82,6 +84,14 @@ namespace MacsASPNETCore
             }
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<ForwardedHeadersOptions>(options => {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                // Azure Subnets
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:10.0.0.0"), 104));
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:192.168.0.0"), 112));
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:172.16.0.0"), 108));
+            });
             services.AddNodeServices();
             services.AddMvcCore()
                 .AddRazorViewEngine();
@@ -102,6 +112,7 @@ namespace MacsASPNETCore
             }
             else
             {
+                app.UseForwardedHeaders();
                 app.UseExceptionHandler("/Home/Error");
             }
 
